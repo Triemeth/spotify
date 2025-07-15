431094c7f46da475f0f978f4b8261617a60ce29b
Ethan Triem <EJTriem@outlook.com>
2025-07-15 17:01:33 -0500

    save everything testing api

--- api_main/api_connect.py
+++ api_main/api_connect.py
@@ -1,4 +1,29 @@
-import requests
-import json
-import base64
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
 
+def get_creditionals():
+    load_dotenv('/app/.env')
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=os.getenv("REDIRECT_URI")
+    ))
+    
+    return sp
+
+if __name__ == "__main__":
+    sp = get_creditionals()
+
+    playlists = sp.user_playlists('spotify')
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print(f"{i + 1 + playlists['offset']:4d} {playlist['uri']} {playlist['name']}")
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+#py -3.12 api_main/api_connect.py
